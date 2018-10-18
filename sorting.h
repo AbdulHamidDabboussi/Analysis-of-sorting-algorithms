cf3cd91012563037ab4c977af90586aee9b5f9cc
Abdul Hamid Dabboussi <37672206+AbdulHamidDabboussi@users.noreply.github.com>
2018-10-18 17:00:23 +0300

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -1,11 +1,3 @@
-/*
-* Title : Algorithm Efficiency and Sorting
-* Author : Abdul Hamid Dabboussi
-* ID : 21701076
-* Section : 2
-* Assignment : 1
-* Description : Insertion, merge, quick and hybrid sorting algorithms for an integer array.
-*/
 #pragma once
 
 void insertionSort(int *arr, int first, int last, int &compCount, int &moveCount);
@@ -17,4 +9,3 @@ void performanceAnalysis(int size);
 void merge(int * arr, int first, int mid, int last, int & compCount, int &moveCount);
 void partition(int * arr, int first, int last, int & pivotIndex, int & compCount, int & moveCount);
 void swap(int & x, int & y);
-
