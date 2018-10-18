71eb73264474beabe05926981230ea4eb38fc285
Abdul Hamid Dabboussi <37672206+AbdulHamidDabboussi@users.noreply.github.com>
2018-10-18 16:59:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,3 @@
-/*
-* Title : Algorithm Efficiency and Sorting
-* Author : Abdul Hamid Dabboussi
-* ID : 21701076
-* Section : 2
-* Assignment : 1
-* Description : Insertion, merge, quick and hybrid sorting algorithms for an integer array.
-*/
-
 #include "sorting.h"
 
 int main() {
